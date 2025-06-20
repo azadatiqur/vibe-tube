c74d856bd72c5ca22f5c17d5142fcff64dc61af8
azadatiqur <mdatiqurrahman.1010@gmail.com>
2025-06-20 07:04:47 +0600

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,11 +3,23 @@
 //there is a solution to this problem, where we use the import statement to import the dotenv package and then call the config method on it. This will allow us to use the environment variables in our code without having to use require. The solution is as follows:
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
+import e from "express";
 dotenv.config({ path: "./.env" });
 
-connectDB();
-
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!!", error);
+  });
 /*
 import express from "express"
 const app = express()
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// try-catch wrapper for async route handlers
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next);
+
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
